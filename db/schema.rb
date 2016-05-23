43553cb53df3f09908a3dc649f00a35e66a84733
emyelabasy <egad86@yahoo.com>
2016-05-23 10:29:38 +0200

    update css

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20160520173058) do
   create_table "friends", force: :cascade do |t|
     t.text     "email",      limit: 65535
     t.integer  "friend_id",  limit: 4
-    t.text     "email",      limit: 65535
     t.integer  "group_id",   limit: 4
     t.integer  "user_id",    limit: 4
     t.datetime "created_at",               null: false
@@ -73,22 +72,18 @@ ActiveRecord::Schema.define(version: 20160520173058) do
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",               limit: 255, default: "",            null: false
-    t.string   "image",                  limit: 255, default: "default.jpg", null: false
-    t.string   "provider",               limit: 255
-    t.string   "uid",                    limit: 255
-    t.string   "email",                  limit: 255, default: "",            null: false
-    t.string   "encrypted_password",     limit: 255, default: "",            null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,             null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                                 null: false
-    t.datetime "updated_at",                                                 null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
